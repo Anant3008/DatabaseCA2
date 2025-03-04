2f1e0104891583e4e3a68130ed9880be3cd533f5
Anant3008 <sastrulaanant@gmail.com>
2025-03-04 09:56:43 +0530

    completed code

---
+++ schema.js
@@ -0,0 +1,34 @@
+const mongoose=require('mongoose')
+
+const RestaurantSchema=new mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    location:{
+        type: String,
+        required: true
+    },
+    cuisine:{
+        type:String,
+        required: true
+    },
+    rating:{
+        type: Number
+    },
+    menu:{
+        name:{
+            type:String,
+            required:true
+        },
+        description:{
+            type: String
+        },
+        price:{
+            type: Number,
+            required: true
+        }
+    }
+})
+
+module.exports=mongoose.model("Restaurant",RestaurantSchema)
---
+++ server.js
@@ -0,0 +1,111 @@
+const express=require('express')
+const mongoose=require('mongoose')
+require('dotenv').config()
+
+const app=express()
+const RestaurantSchema=require('./schema')
+
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on http://localhost:${process.env.PORT}`)
+})
+
+//Connecting to MONGO_DB DATABASE
+mongoose.connect(process.env.MONGO_URI)
+.then(()=> console.log("Connected to Database"))
+.catch((err)=> console.log("Error",err))
+
+//CRUD OPERATIONS
+
+app.get('/resto',async (req,res)=>{
+    try{
+    const data= await RestaurantSchema.find()
+
+    if(!data){
+        res.status(404).json({message: "No data found"})
+    }
+
+    res.status(200).json(data)
+}catch(err){
+    res.status(500).json({message: "Something went wrong"})
+}
+})
+
+app.get('/resto/:id',async (req,res)=>{
+    try{
+    const data= await RestaurantSchema.findById(req.params.id)
+
+    if(!data){
+        res.status(404).json({message: "RestaurantSchema not found"})
+    }
+
+    res.status(200).json(data)
+}catch(err){
+    res.status(500).json({message: "Something went wrong"})
+}
+})
+
+app.post('/resto',async(req,res)=>{
+    try {
+        const {name,location,cuisine,rating,menu}=req.body
+
+        if(!name){
+            res.status(400).json({message: " Validation failed name is required"})
+        }
+        if(!location){
+            res.status(400).json({message: " Validation failed location is required"})
+        }
+        if(!cuisine){
+            res.status(400).json({message: " Validation failed cuisine is required"})
+        }
+        if(!menu.price){
+            res.status(400).json({message: " Validation failed menu price is required"})
+        }
+        if(!menu.name){
+            res.status(400).json({message: " Validation failed menu name is required"})
+        }
+        
+
+        const newItem=new RestaurantSchema({name,location,cuisine,rating,menu})
+        await newItem.save()
+
+        res.status(201).json({message : "created successfully",item:newItem})
+        
+
+    } catch (error) {
+        res.status(500).json({message: "Something went wrong"})
+    }
+})
+
+app.put('/resto/:id',async(req,res)=>{
+    try {
+        const {name,location,cuisine,rating,menu}=req.body
+
+        const updateItem=await RestaurantSchema.findByIdAndUpdate(req.params.id,{name,location,cuisine,rating,menu})
+
+        if(!updateItem){
+            res.status(404).json({message: "RestaurantSchema not found"})
+        }
+        await updateItem.save()
+
+        
+        res.status(200).json({message: "Updated successfully",item: updateItem})
+
+    } catch (error) {
+        res.status(500).json({message: "Something went wrong"})
+    }
+})
+
+app.post('/resto/:id',async(req,res)=>{
+    try {
+        const deletedItem=await RestaurantSchema.findByIdAndDelete(req.params.id)
+
+        if(!deletedItem){
+            res.status(404).json({message: "RestaurantSchema not found"})
+        }
+
+        res.status(200).json({message: "RestaurantSchema Deleted Successfully"})
+    } catch (error) {
+        res.status(500).json({message: "Something went wrong"})
+    }
+})
+
